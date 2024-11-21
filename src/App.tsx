e2874b644a9273e940c53f668842382d9f26bb42
woodzverse <woodzverse@naver.com>
2024-11-21 20:53:08 +0900

    chore: App.tsx 세팅(기본 페이지 생성)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Alarm from "./pages/Alarm";
+import Main from "./pages/Main";
+import RegisterPet from "./pages/RegisterPet";
+import AIMatching from "./pages/AIMatching";
+import Notice from "./pages/Notice";
+import Facilities from "./pages/Facilities";
+import WalkingCourse from "./pages/WalkingCourse";
+import MyWalkingCourse from "./pages/MyWalkingCourse";
+import MyInfo from "./pages/MyInfo";
+import Preferences from "./pages/Preferences";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Header /> {/* 헤더 */}
+      <Routes>
+        <Route path="/" element={<Main />} /> {/* 메인 페이지 */}
+        <Route path="/login" element={<Login />} /> {/* 로그인 페이지 */}
+        <Route path="/signup" element={<Signup />} /> {/* 회원가입 페이지 */}
+        <Route path="/alarm" element={<Alarm />} /> {/* 알람 페이지 */}
+        <Route path="/register-pet" element={<RegisterPet />} /> {/* 반려동물 등록 페이지 */}
+        <Route path="/ai-matching" element={<AIMatching />} /> {/* AI 매칭 페이지 */}
+        <Route path="/notice" element={<Notice />} /> {/* 공지사항 페이지 */}
+        <Route path="/facilities" element={<Facilities />} /> {/* 반려동물 관련 시설 페이지 */}
+        <Route path="/walking-course" element={<WalkingCourse />} /> {/* 산책 코스 페이지 */}
+        <Route path="/my-walking-course" element={<MyWalkingCourse />} /> {/* 나의 산책 코스 페이지 */}
+        <Route path="/my-info" element={<MyInfo />} /> {/* 내정보 페이지 */}
+        <Route path="/preferences" element={<Preferences />} /> {/* 선호동물 입력 및 수정 페이지 */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
