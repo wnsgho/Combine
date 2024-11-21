7ca74375ca29e02ed49891ebf5e9cc5c2e5e8721
한지원 <gomdeschool@naver.com>
2024-11-21 19:28:37 +0900

    상세작성페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './index.css'
 
 import MatchingPage from './pages/matching/MatchingPage'
+import DetailPage from './pages/matching/DetailPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <MatchingPage />
+      <DetailPage />
     </>
   )
 }
