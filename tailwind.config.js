a16f419a82cfdb9f6d5329be521d2718b50198c9
gyu <qkr9748200@gmail.com>
2024-11-26 12:33:06 +0900

    fix:css적용문제 해결, 포스트 작성페이지 오류 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
+    extend: {colors: {
       mainColor: '#AB654B',
       cancelColor: '#6366FF'
-    },
+    },},
+    
   },
   plugins: [require("tailwind-scrollbar-hide"),],
 };
